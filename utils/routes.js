cb86c451ac190c71dbca30ac42872e81be5cda7d
Samarth Dengre <103602874+Samarth-Dengre@users.noreply.github.com>
2023-02-14 11:22:13 +0530

    Update routes.js

--- utils/routes.js
+++ utils/routes.js
@@ -1,4 +1,3 @@
 export const host = "https://moviemate.cyclic.app";
-// export const host = "http://172.22.29.85:3000";
 export const auth = `${host}/auth`;
 export const moviesRoute = `${host}/movies`;
